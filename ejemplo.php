bbdbcf015109c6f8bbd2ea777150fce18c22b2a1
0V3RW4TCHz <3duardorivasme@gmail.com>
2025-05-20 00:28:32 -0600

    a ver

--- ejemplo.php
+++ ejemplo.php
@@ -3,5 +3,9 @@
 
 ?><!DOCTYPE html>
 <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de PHP</title>
 
 </html>
